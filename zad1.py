78c5d1ad9b4a6d876f50f8e399f61e5a58142651
ASIEK <89014321+asiekasiek@users.noreply.github.com>
2022-04-28 05:07:12 +0200

    Create zad1.py

---
+++ zad1.py
@@ -0,0 +1,12 @@
+#Twoim zadaniem jest przygotowanie prostego kodu, który będzie w stanie wskazać czas zakończenia dla jakiegoś przedziału czasu, podanego jako liczba minut (może być dowolnie duża). Czas rozpoczęcia podawany jest jako para godzin (0..23) i minut (0..59). Wynik musi zostać wyświetlony na konsoli.
+
+#Na przykład, jeśli wydarzenie zaczyna się o 12:17 i trwa 59 minut, to skończy się o 13:16.
+
+h = int(input("Czas startu (godziny): "))
+m = int(input("Czas startu (minuty): "))
+d = int(input("Czas trwania wydarzenia (minuty): "))
+
+# wprowadź tutaj swój kod
+km = ((m + d)%60) #obliczenie minuty końca wydarzenia
+kg = (int(h+(m+d)/60))%24 #obliczenie godziny końca wydarzenia
+print(kg, ":", km)
